f907c37855da0141bae70d48507d355d76988047
nadering <120453768+nadering@users.noreply.github.com>
2023-07-19 07:13:29 +0900

    Chore: Modified adorable-css order
    Modified adorable-css order from sidebar component
    
    Adorable-css order (left-to-right):
    - flexbox (vbox, hbox, pack, center, ...)
    - position (relative, absolute, width, height, ...)
    - color (color, background-color, ...)
    - border (border, border-radius, ...)
    - font (font-size, font-weight, ...)
    - padding
    - margin
    - gap
    - others (transition, nowrap, ...)
    - flex-option (flex-basis, flex-grow, ...)
    - prefix (hover, active, ...)
    - media-query

--- src/components/workspace/navigation/search.tsx
+++ src/components/workspace/navigation/search.tsx
@@ -2,10 +2,10 @@ export default function Search() {
   return (
     <div className="hbox pack relative flex-basis(40%) flex-grow(1) @w(~1440):w(~none) @w(1440~):w(~700px)">
       <button
-        className="hbox space-between flex-basis(40%) r(4) px(8) py(2)
-        bg(--search-background) flex-grow(1) hover:bg(--search-background-hover)"
+        className="hbox space-between bg(--search-background) r(4) px(8) py(2)
+        flex-basis(40%) flex-grow(1) hover:bg(--search-background-hover)"
       >
-        <span className="c(--search-text) bold font(14) translateY(1px) nowrap...">
+        <span className="c(--search-text) font(14) bold translateY(1px) nowrap...">
           {/* Workspace name */}
           {"테스트 워크스페이스 검색"}
         </span>
